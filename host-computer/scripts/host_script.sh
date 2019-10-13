b312661cd4ca508f10f024000b6fcd716e41e5a5
kep1616 <kylepike@gmail.com>
2019-10-13 10:49:09 -0400

    makefile typo

---
+++ host-computer/scripts/host_script.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#Switch to user Participants will use
+sudo su -l ec2-user
+
+#Update host
+sudo yum update -y
+
+#Get Terraform
+sudo yum install unzip -y
+wget https://releases.hashicorp.com/terraform/0.12.10/terraform_0.12.10_linux_amd64.zip
+
+#Install Terraform
+unzip terraform_0.12.10_linux_amd64.zip
+sudo mv terraform /usr/local/bin/
+
+#Remove downloaded Terraform zip
+rm terraform_0.12.10_linux_amd64.zip
+
+#Install Git
+sudo yum install git-core -y
+
+#Download Demo Files
+git clone https://github.com/kep1616/terraform-demo.git /home/ec2-user/terraform-demo
